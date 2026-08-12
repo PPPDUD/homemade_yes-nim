409d0e2a4a42ae3b9351e9dfb33700ff5c24a646
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2026-08-11 20:30:21 -0400

    Fix logic bug

--- src/homemade_yes.nim
+++ src/homemade_yes.nim
@@ -5,11 +5,13 @@ proc handle_interruption*() {.noconv.} =
 
 var str = join(commandLineParams(), " ")
 if str.len == 0:
-    str = "y\n"
+  str = "y\n"
 else:
-    str.add("\n")
-let repeated = str.repeat(16384 div str.len)
+  str.add("\n")
+
+let repeated = (if str.len >= 16384: str else: str.repeat(16384 div str.len))
+
 setControlCHook(handle_interruption)
 while true:
-    stdout.write(repeated)
-    stdout.flushFile()
+  stdout.write(repeated)
+  stdout.flushFile()
