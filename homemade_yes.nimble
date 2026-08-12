573af0499419d1a718dca12669bdee879945cfdd
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2026-08-11 20:16:56 -0400

    First commit

---
+++ homemade_yes.nimble
@@ -0,0 +1,13 @@
+# Package
+
+version       = "0.1.0"
+author        = "PPPDUD"
+description   = "Port of homemade_yes for Nim."
+license       = "MIT"
+srcDir        = "src"
+bin           = @["homemade_yes"]
+
+
+# Dependencies
+
+requires "nim >= 2.2.4"
---
+++ src/homemade_yes.nim
@@ -0,0 +1,15 @@
+import std/os, std/strutils
+
+proc handle_interruption*() {.noconv.} =
+  quit(0)
+
+var str = join(commandLineParams(), " ")
+if str.len == 0:
+    str = "y\n"
+else:
+    str.add("\n")
+let repeated = str.repeat(16384 div str.len)
+setControlCHook(handle_interruption)
+while true:
+    stdout.write(repeated)
+    stdout.flushFile()
